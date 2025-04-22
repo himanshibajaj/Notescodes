9e98d750988fe194479eff3dacfbe39318b0084f
Himanshi Bajaj <himanshibajaj723@gmail.com>
2025-04-22 23:34:40 +0530

    Create W5App.js

---
+++ W5App.js
@@ -0,0 +1,17 @@
+//npx create-react-app react-form-app
+//cd react-form-app
+//npm start
+
+import React from 'react';
+import './App.css';
+import FormComponent from './FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
